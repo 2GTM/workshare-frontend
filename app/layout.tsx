5ca56ec190ced97ba20187267af0c5516bf7b910
guettafa <irontitou5@gmail.com>
2024-07-12 11:45:02 -0400

    Changed layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <body>
+        <nav>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/about"}>About</Link>
+        </nav>
+      </body>
       <body className={inter.className}>{children}</body>
     </html>
   );
