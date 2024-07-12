93d428b25f0b63b5d5ff394386f4b56a65be3615
AchrafGroiez <41470d0a@gmail.com>
2024-07-12 18:53:12 -0400

    Added ClientService and advanced project creation.

---
+++ components/shared/FormikInput.tsx
@@ -0,0 +1,25 @@
+import { TextField } from "@mui/material";
+import { ErrorMessage, FieldProps } from "formik";
+import { HTMLInputTypeAttribute } from "react";
+
+type FormikInputProps = {
+    multiline?: boolean;
+    type?: HTMLInputTypeAttribute;
+    label: string;
+}
+
+export default function FormikInput(props: FormikInputProps & FieldProps) {
+    const { name } = props.field;
+
+    return (
+        <TextField
+            {...props.field}
+            label={props.label}
+            type={props.type}
+            multiline={props.multiline}
+            rows={props.multiline ? 5 : undefined}
+            error={!!props.form.errors[name] && Boolean(props.form.touched[name])}
+            helperText={<ErrorMessage name={name} />}
+        />
+    );
+};
