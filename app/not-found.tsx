965ff92d80c88e6b1b66f274111cee962fca61f9
QuocDungTran380 <kevintran380@gmail.com>
2024-07-14 04:52:34 -0400

    fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,9 +4,9 @@ import TextTypeWriter from "@/components/shared/TypeWriterText";
 export default function NotFound() {
     return (
         <Container>
-            <Typography fontSize={450} textAlign={"center"}>
+            <span style={{ fontSize: "450px", textAlign: "center"}}>
                 <TextTypeWriter text={"404"} />
-            </Typography>
+            </span>
             <Typography textAlign="center" variant="h2">You are not supposed to be there</Typography>
         </Container>
     )
