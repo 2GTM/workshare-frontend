9a27a87d6663478e8de802c3094fbae59bf4ca4e
AchrafGroiez <41470d0a@gmail.com>
2024-07-14 00:43:34 -0400

    Started login page.

---
+++ app/login/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import FormikInput from "@/components/shared/FormikInput";
+import { Button, Container, Grid, Stack, Typography } from "@mui/material";
+import { Field, Form, Formik } from "formik";
+import Image from "next/image";
+
+export default function Login() {
+    return (
+        <Container>
+
+            <Formik
+                initialValues={{
+                    username: "",
+                    password: ""
+                }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                <Form>
+                    <Stack alignItems="center" justifyContent="center" flexWrap="wrap" direction="row" spacing={2} useFlexGap>
+                        <Stack width={500} spacing={2} useFlexGap flexWrap="wrap">
+                            <Typography variant="h3">Login</Typography>
+                            <Field name="username" component={FormikInput} label="Username" />
+                            <Field name="password" component={FormikInput} label="Password" />
+
+                            <Button variant="contained">Sign in</Button>
+                        </Stack>
+
+                        <Image alt="arduino" src="/arduino.avif" width={550} height={600} />
+                    </Stack>
+                </Form>
+            </Formik>
+        </Container>
+    );
+}
