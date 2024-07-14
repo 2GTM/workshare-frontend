2909720b9a3c69e56aa5559d57db4f0aebe7c42d
QuocDungTran380 <kevintran380@gmail.com>
2024-07-14 02:05:12 -0400

    added profile page

--- app/clients/[username]/page.tsx
+++ app/clients/[username]/page.tsx
@@ -2,21 +2,23 @@ import { Avatar, Typography } from "@mui/material";
 import ClientInfo from "@/models/ClientDto";
 import { getClientInfo } from "@/services/ClientService";
 import Box from '@mui/material/Box';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from "next/link";
+import { Link as MuiLink } from "@mui/material";
 
 export default async function ClientPage({ params }: any) {
-
     const client: ClientInfo = (await getClientInfo(params.username)).data;
-
     return (
         <>
-            <Box
-                height={1000}
-                width={500}
-                borderRadius={2}
-                sx={{ border: '2px solid grey' }}
-                margin={"auto"}
-                paddingTop={5}
-            >
+            {client ?
+                <Box
+                    height={1000}
+                    width={500}
+                    borderRadius={2}
+                    sx={{ border: '2px solid grey' }}
+                    margin={"auto"}
+                    paddingTop={5}
+                >
                     <Box
                         borderRadius={2}
                         height={350}
@@ -24,35 +26,45 @@ export default async function ClientPage({ params }: any) {
                         margin={"auto"}
                     >
                         <Avatar
-                        sx={{ width: 350, height: 350}}
-                        variant="rounded"
+                            sx={{ width: 350, height: 350 }}
+                            variant="rounded"
                         >
 
                         </Avatar>
                     </Box>
                     <Box
-                    marginLeft={9}
-                    marginTop={5}
-                    marginRight={5}
+                        marginLeft={9}
+                        marginTop={5}
+                        marginRight={5}
                     >
-                    <Typography
-                        variant="h3"
-                        textAlign={"left"}
-                    >Username</Typography>
-                    <Typography
-                        variant="h6"
-                        textAlign={"left"}
-                        color={"#bbbbbb"}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pretium purus et enim ultricies, eget ultricies velit vehicula. Pellentesque id mollis ex, eu suscipit mauris.
-                    </Typography>
-                    <Typography
-                    marginTop={5}
-                    fontWeight={"bold"}
-                    >
-                        IS WORKING ON 5 PROJECTS
-                    </Typography>
+                        <Typography
+                            fontWeight={"700"}
+                            variant="h3"
+                            textAlign={"left"}
+                        >{client.username}&nbsp;
+                            <MuiLink component={Link} href={client.github}><GitHubIcon fontSize="large"/></MuiLink>
+                        </Typography>
+                        <Typography
+                            fontWeight={"700"}
+                            variant="h6"
+                            textAlign={"left"}
+                            color={"#bbbbbb"}>
+                            {client.bio}
+                        </Typography>
+                        <Typography
+                            marginTop={5}
+                            fontWeight={"bold"}
+                        >
+                            IS WORKING ON {client.projectsCount} PROJECT(S)
+                        </Typography>
                     </Box>
-            </Box>
+                </Box>
+                :
+                <Typography
+                    variant="h2"
+                    textAlign="center"
+                >The user is not found</Typography>
+            }
         </>
     );
 }
