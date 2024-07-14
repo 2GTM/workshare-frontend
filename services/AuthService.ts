b8469e02a20f892e84512ec1cf3e27d74e1e5418
AchrafGroiez <41470d0a@gmail.com>
2024-07-14 02:06:08 -0400

    Putted login in components/auth.

---
+++ services/AuthService.ts
@@ -0,0 +1,11 @@
+import http from "@/http";
+
+const REQUEST_MAPPING = "/api/auth";
+
+export function signUp(username: string, password: string) {
+    return http.post(`${REQUEST_MAPPING}/sign-up`, undefined, { params: { username, password } });
+}
+
+export function login(username: string, password: string) {
+    return http.post(`${REQUEST_MAPPING}/login`, undefined, { params: { username, password } });
+}
