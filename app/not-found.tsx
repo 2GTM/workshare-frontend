a05454fa037b158f14e8bf2a3e6898ec21d72480
QuocDungTran380 <kevintran380@gmail.com>
2024-07-14 04:33:41 -0400

    fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,14 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Divider, Stack, Typography } from "@mui/material";
+import TextTypeWriter from "@/components/shared/TypeWriterText";
 
 export default function NotFound() {
     return (
-        <Typography textAlign="center" variant="h1">You are not supposed to be there</Typography>
+        <Container>
+            <Typography fontSize={450} textAlign={"center"}>
+                <TextTypeWriter text={"404"} />
+            </Typography>
+            <Typography textAlign="center" variant="h2">You are not supposed to be there</Typography>
+        </Container>
     )
+
 }
